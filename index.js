bcb6bef0e7a508f5b13ef9207e0bff029752c87b
Nishit  Bhardwaj <nishitbhardwaj11@gmail.com>
2022-12-12 12:15:04 +0530

    little adjust

--- index.js
+++ index.js
@@ -1,12 +1,41 @@
-// var tablinks = document.getElementsByClassName("tab-links");
-// var tabcontents = document.getElementsByClassName("tab-contents");
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
 
-// function opentab(tabname) {
-//   for (tablink of tablinks) {
-//     tablink.classList.remove("active-link");
-//   }
-//   for (tabcontent of tabcontents) {
-//     tabcontent.classList.remove("active-tab");
-//   }
-//   event.currentTarget.classList.add("active-link");
-// }
+function opentab(tabname) {
+  for (tablink of tablinks) {
+    tablink.classList.remove("active-link");
+  }
+  for (tabcontent of tabcontents) {
+    tabcontent.classList.remove("active-tab");
+  }
+  event.currentTarget.classList.add("active-link");
+  document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu() {
+  sidemenu.style.right = "0";
+}
+
+function closemenu() {
+  sidemenu.style.right = "-200px";
+}
+
+const scriptURL =
+  "https://script.google.com/macros/s/AKfycbxrx6rnMlNKy9gAvQLtOcOsKGhF7zy_gAXJI4cbi_3ymEcSBdxlAjPJsz1XeMbU9yVj/exec";
+const form = document.forms["submit-to-google-sheet"];
+const msg = document.getElementById("msg");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      msg.innerHTML = "Message Sent Succefully";
+      setTimeout(function () {
+        innerHTML = "";
+      }, 5000);
+      form.reset();
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
